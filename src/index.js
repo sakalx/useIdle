92ae70880130218b082e21a73a6f368e64e23fee
serhii.sakal <serhii.sakal@axoni.com>
2020-08-24 13:22:42 -0400

    draft version useIdleCallback
    Signed-off-by: serhii.sakal <serhii.sakal@axoni.com>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,45 @@
-const foo = () => {
-    console.log('magic here !!');
-}
+import { useLayoutEffect, useRef, useCallback } from 'react';
 
-export default foo;
+const debounce = (callback, wait = 66) => {
+    let timeout = null;
+
+    return (...args) => {
+        const next = () => callback(...args);
+
+        clearTimeout(timeout);
+        timeout = setTimeout(next, wait);
+    };
+};
+
+const useCancelIdleCallback = () => useCallback((idleId) => clearTimeout(idleId.current), []);
+
+const useIdleCallback = (callback, {idleTimeout = 3000, debounceTimeout = 66} = {}) => {
+    const idleId = useRef(null)
+    const cancelIdle = useCancelIdleCallback();
+
+    useLayoutEffect(() => {
+
+        const resetTimer = debounce(() => {
+            cancelIdle(idleId);
+            idleId.current = setTimeout(callback, idleTimeout);
+        }, debounceTimeout);
+
+        window.onclick = resetTimer;
+        window.onkeypress = resetTimer;
+        window.onmousedown = resetTimer;
+        window.onmousemove = resetTimer;
+        window.ontouchstart = resetTimer;
+
+        return () => {
+            cancelIdle(idleId);
+
+            window.onclick = null;
+            window.onkeypress = null;
+            window.onmousedown = null;
+            window.onmousemove = null;
+            window.ontouchstart = null;
+        };
+    }, []);
+
+    return idleId;
+}
